bbe1c1cffab27e72b82f0a1b31b6b4c7841c8e5e
tsahan <tugcesahan@gmail.com>
2023-05-10 17:44:24 -0400

    Fix batching issue

--- send.py
+++ send.py
@@ -18,8 +18,8 @@ import logging
 import ast
 
 # Notes: Remove unneccessary packages
-EVENT_HUB_CONNECTION_STR = "Endpoint="
-EVENT_HUB_NAME = ""
+EVENT_HUB_CONNECTION_STR = "Endpoint=sb://dataingestiondh.servicebus.windows.net/;SharedAccessKeyName=Allrights;SharedAccessKey=9ZlRnLrrxAfDkMMU6KrEDyTON2NMCi7jx+AEhG4HBJY=;EntityPath=testeventhub"
+EVENT_HUB_NAME = "testeventhub"
 BATCH_SIZE = 1000
    
 async def run():
@@ -31,25 +31,30 @@ async def run():
     )
 
     event_count = 0
+    file_count = 0
+    total_lines = 0
     async with producer:
         # Root folder
-        root_folder = ""
-        log_file = ""
+        root_folder = "C:\\Users\SESA692481\\OneDrive - Schneider Electric\\Code\\DHProdRestore\\files\\temp"
+        log_file = "C:\\Users\SESA692481\\OneDrive - Schneider Electric\\Code\\DHProdRestore\\output.txt"
         
         for subdir, dirs, files in os.walk(root_folder):
             for file in files:
+                file_count += 1
                 mfilename = os.path.join(subdir, file)
                 msg_filename = mfilename
                 # Create a batch.
                 event_data_batch = await producer.create_batch()
                 # can we add more events to the existing batch
                 # Add events to the batch.
-                batch =0
-                y=0
+                # batch = 0
+                num_lines = 0
+                y = 0
                 # msg_filename ="files/0.txt"
                 with open(msg_filename,'r') as f:
-                    print('\033[32mFile name: \033[0m', msg_filename)
+                    print('\033[33mFile name: \033[0m', msg_filename)
                     for line in f:
+                        num_lines += 1
                         try:
                             msg_str = line.replace ("b'", "'")
                             msg_str_body = msg_str[msg_str.find ('{ body:')+7 : msg_str.find (", properties:")]
@@ -60,20 +65,25 @@ async def run():
                                 msg_str_properties = msg_str_properties.replace ('UUID(','"UUID(') 
                                 #msg_str_properties = msg_str_properties[:msg_str_properties.find ('"UUID(')] + '"' +  msg_str_properties[msg_str_properties.find (', DSP-Response-Code:')+45:] 
                                 msg_str_properties = msg_str_properties[:msg_str_properties.find (", 'DSP-Request-Type':")] + '"' + msg_str_properties[msg_str_properties.find (", 'DSP-Request-Type':"):]
-                            batch +=1
-                            if (batch <= BATCH_SIZE):
+                            # batch +=1
+                            if (len(event_data_batch) < BATCH_SIZE):
                                 event = EventData(body= msg_str_body)
                                 # print(event)
-                                event.properties =ast.literal_eval(msg_str_properties)
+                                event.properties = ast.literal_eval(msg_str_properties)
                                 event_data_batch.add(event)
                                 event_count += 1
                             else:
-                                print('\033[32mBatch size is: \033[0m', len(event_data_batch))
+                                print('\033[32mBatch size: \033[0m', len(event_data_batch))
                                 await producer.send_batch(event_data_batch)    
-                                event_data_batch = await producer.create_batch()                               
-                                batch =0
+                                event_data_batch = await producer.create_batch()
+                                event = EventData(body= msg_str_body)
+                                event.properties = ast.literal_eval(msg_str_properties)
+                                event_data_batch.add(event)
+                                event_count += 1
+                                # batch = 0
                         except Exception as err:
                             with open(log_file, 'a') as log_f:
+                                print("We're in the exception")
                                 print(str(err))
                                 log_f.write ('Error encountered while processing:' + msg_str + 'in the file' + msg_filename + '\n')
                                 log_f.write (str(err))
@@ -82,25 +92,30 @@ async def run():
                                 log_f.close()
                             pass 
                         # continue
-                    if batch > 0:
-                        print('\033[32mBatch size is: \033[0m', len(event_data_batch))
+                    if len(event_data_batch) > 0:
+                        print('\033[32mBatch size: \033[0m', len(event_data_batch))
                         await producer.send_batch(event_data_batch)    
                         event_data_batch = await producer.create_batch()                               
-                        batch =0
+                        # batch = 0
                     f.close()
                 with open(log_file, 'a') as log_f:
                     output_time = str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
                     log_f.write (output_time + ' Completed processing: ' + msg_filename + '\n')
                     log_f.close()
                 # quit()
-    print('\033[32mTotal num of events: \033[0m', event_count)
+                print('\033[32mNumber of lines in the file:\033[0m', num_lines)
+                total_lines += num_lines
+    print('\033[35mTotal num of events: \033[0m', event_count)
+    print('\033[35mTotal num of lines: \033[0m', total_lines)
+    print('\033[35mTotal num of files: \033[0m', file_count)
 
 startTime = datetime.now()
-print('\033[32mThe script has started running at \033[0m', startTime)
+print('The script has started running at ', startTime)
 loop = asyncio.get_event_loop()
 loop.run_until_complete(run())
-print('\033[32mThe script has finished running at \033[0m', datetime.now())
-print('\033[32mTotal processing time in seconds: \033[0m', datetime.now() - startTime)
+print('\033[35mTotal processing time in seconds: \033[0m', datetime.now() - startTime)
+print('The script has finished running at ', datetime.now())
+
 
 
 
